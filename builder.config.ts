9162fd0d74090244441d43884a168d816f185ffb
ChrisSong1994 <18557511748@163.com>
2024-12-26 14:33:39 +0800

    chore: 构建基础配置

--- builder.config.ts
+++
@@ -1,7 +0,0 @@
-// rsbuild 构建配置项 https://rsbuild.dev/zh/config/index
-
-export default {
-  output: {
-    assetPrefix: "auto",
-  },
-};
--- rspress.config.ts
+++ rspress.config.ts
@@ -1,15 +1,21 @@
 import * as path from "node:path";
 import { defineConfig } from "rspress/config";
-import builderConfig from "./builder.config";
+
+const BASE_PATH = "/frontend-stack/";
 
 export default defineConfig({
   root: path.join(__dirname, "docs"),
   title: "FRONTEND STACK",
   icon: "/logo.icon.png",
   logo: "/logo.jpeg",
-  base: '/frontend-stack/',
+  base: BASE_PATH,
   logoText: "Frontend Stack",
-  builderConfig: builderConfig,
+  builderConfig: {
+    output: {
+      assetPrefix: BASE_PATH,
+    },
+  },
+  ssg: false,
   themeConfig: {
     socialLinks: [
       {
